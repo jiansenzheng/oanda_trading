d0d3c5e8f4e1c1c667dfd31be3c4d481e86dc629
Jiansen Zheng <jiansendhu@126.com>
2017-10-11 14:38:24 +0200

    modified the logging configuration

--- oanda_trading/trading_log.py
+++ oanda_trading/trading_log.py
@@ -1,14 +1,14 @@
 log_dict = {
 			"EUR_USD":
 				{"name": 'eur_usd.log',
-				"path": '/home/zheng/data/trading/EUR_USD/'
+				"path": '/home/EUR_USD/'
 				},
 			"USD_CNH":
 				{"name": 'usd_cnh.log',
-				"path": '/home/zheng/data/trading/USD_CNH/'
+				"path": '/home/USD_CNH/'
 				},
 			"AUD_USD":
 				{"name": 'aud_usd.log',
-				"path": '/home/zheng/data/trading/AUD_USD/'
+				"path": '/home/AUD_USD/'
 				}
 		   }
