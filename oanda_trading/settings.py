380729345a902a2079019a27d45949134046272d
Jiansen Zheng <jiansendhu@126.com>
2017-10-11 22:21:27 +0200

    add the missing settings for trading/streaming API

---
+++ oanda_trading/settings.py
@@ -0,0 +1,18 @@
+ENVIRONMENTS = {
+    "streaming": {
+        "real": "stream-fxtrade.oanda.com",
+        "practice": "stream-fxpractice.oanda.com",
+        "sandbox": "stream-sandbox.oanda.com"
+    },
+    "api": {
+        "real": "api-fxtrade.oanda.com",
+        "practice": "api-fxpractice.oanda.com",
+        "sandbox": "api-sandbox.oanda.com"
+    }
+}
+
+DOMAIN = "practice"
+STREAM_DOMAIN = ENVIRONMENTS["streaming"][DOMAIN]
+API_DOMAIN = ENVIRONMENTS["api"][DOMAIN]
+ACCESS_TOKEN = '' 
+ACCOUNT_ID = ''
